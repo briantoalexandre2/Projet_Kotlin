f54b99550b9f3621974ed4a7d57e48ab233fe0e3
trojan <trojan.abuser@gmail.com>
2026-04-07 21:21:53 +0200

    partie 2

--- src/Joueur.kt
+++ src/Joueur.kt
@@ -1,27 +1,26 @@
-import kotlin.collections.minusAssign
-import kotlin.compareTo
-
-class Joueur(val nom: String, var vie: Int, var armeEquipee: Arme?, inventaire: MutableList<Arme> = mutableListOf()) {
+class Joueur(val nom: String, var vie: Int, var armeEquipee: Arme, inventaire: MutableList<Arme> = mutableListOf(Arme("Poing", 5))) {
     var inventaire = inventaire
-    constructor(nom: String, vie: Int, inventaire: MutableList<Arme> ): this(nom, vie, armeEquipee = null, inventaire) {
+    constructor(nom: String, vie: Int, inventaire: MutableList<Arme> ): this(nom, vie, armeEquipee = Arme("Poing", 5), inventaire) {
         this.vie = vie
-        this.armeEquipee = null
+        this.armeEquipee = armeEquipee
         this.inventaire = inventaire
 
     }
 
-    fun attaquer(): Int {return this.armeEquipee!!.degat}
-    fun recevoirDegats(degat: Int) {if (this.vie>0) this.vie-=degat}
-    fun ajouterArme(arme: Arme) {this.inventaire.add(arme)}
-    fun changerArme(nomArme: String) {
-        val boolTest = this.inventaire.find {it.nom == nomArme}
-        if (boolTest != null) {
+    fun attaquer(): Int {return this.armeEquipee.degat}
+    fun recevoirDegats(degat: Int): Unit {if (this.vie>0) this.vie-=degat}
+    fun ajouterArme(arme: Arme): Unit {this.inventaire.add(arme)}
+    fun changerArme(nomArme: String): Unit {
+        val arme = this.inventaire.find {it.nom.lowercase() == nomArme.lowercase()}
+        if (arme != null) {
+
             this.inventaire.add(this.armeEquipee)
-        }
-
-
+            this.inventaire.remove(arme)
+            this.armeEquipee = arme
+            println("${this.nom} a équipé ${this.armeEquipee}")
+        } else {println("Arme non trouvée dans l'inventaire")}
     }
-    fun afficherInventaire() {}
+    fun afficherInventaire(): Unit {this.inventaire.forEach { println(it) }}
 
     override fun toString(): String {
         return "($nom, vie=$vie, armeEquipee=$armeEquipee, inventaire=$inventaire)"
--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,13 @@
 val arme1: Arme = Arme("épée", 15)
-val arme2: Arme = Arme("arc", 10)
+val arme2: Arme = Arme("Arc", 12)
+val arme3: Arme = Arme("Hache", 20)
 
-val j1: Joueur = Joueur("alex", 20, mutableListOf(arme2))
-val j2: Joueur = Joueur("alex", 20, arme2,)
-val j3: Joueur = Joueur("alex", 20, arme2,mutableListOf(arme2))
+val j1: Joueur = Joueur("alex", 20, mutableListOf(arme2, arme1)) /*Test*/
+val j2: Joueur = Joueur("Arthur", 50, arme1)
+val j3: Joueur = Joueur("Lancelot", 50, mutableListOf())
 
 fun main() {
     fun etape1() {
-
         println(arme1)
         println(arme2)
         arme2.degat = 12
@@ -16,9 +16,22 @@ fun main() {
         println(arme2)
     }
     fun etape2() {
-        println(j1)
-        println(j2)
+        /*Artuhr*/
+//        println(j2)
+//        j2.ajouterArme(arme2)
+//        j2.ajouterArme(arme3)
+//        j2.afficherInventaire()
+//
+//        j2.changerArme("Hache")
+//        j2.changerArme("Lance")
+
+        /*Lancelot*/
         println(j3)
+        j3.ajouterArme(arme2)
+        j3.changerArme("Arc")
+        println("${j3.nom} est équipé de l'arme : ${j3.armeEquipee}")
+        j3.afficherInventaire()
+
 
     }
     etape2()
